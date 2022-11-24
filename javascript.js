b5618631f59e1604c9ffb76400bc27e5a6c7f790
sebidelamata <sebidelamata@gmail.com>
2022-11-24 12:02:09 -0700

    made rounds and game, some output messages too

--- javascript.js
+++ javascript.js
@@ -1,4 +1,10 @@
 
+// some global vars well reference later
+let outputMessage;
+let playerSelection;
+let winCount;
+let loseCount;
+
 // function to randomly return either rock paper or scissors
 function getComputerChoice(){
     let decider =Math.random() * 3;
@@ -12,6 +18,7 @@ function getComputerChoice(){
     }
 }
 
+// play a single round takes a string outputs a string, increments wincount losecount, or neither (tie)
 function playRound(playerSelection){
     if(typeof(playerSelection) != "string"){
         return;
@@ -23,40 +30,64 @@ function playRound(playerSelection){
     switch(cleanedInput){
         case "ROCK":
             if(computerChoice == "Paper"){
+                loseCount++;
                 return "You lose! Paper beats Rock.";
-                break;
             } else if(computerChoice == "Scissors"){
+                winCount++;
                 return "You win! Rock beats Scissors.";
-                break;
             } else if(computerChoice == "Rock"){
                 return "A tie! You both drew Rock.";
-                break;
             }
         case "PAPER":
             if(computerChoice == "Paper"){
                 return "A tie! You both drew paper.";
-                break;
             } else if(computerChoice == "Scissors"){
+                loseCount++;
                 return "You Lose! Scissors beats Paper.";
-                break;
             } else if(computerChoice == "Rock"){
+                winCount++;
                 return "You win! Paper covers rock.";
-                break;
             }
         case "SCISSORS":
             if(computerChoice == "Paper"){
+                winCount++;
                 return "You win! Scissors beats Paper.";
-                break;
             } else if(computerChoice == "Scissors"){
                 return "A tie! You both drew Scissors.";
-                break;
             } else if(computerChoice == "Rock"){
+                loseCount++;
                 return "You lose! Rock beats scissors.";
-                break;
             }
     }
 }
 
-let playerSelection = "scissors";
+// play a game
+function playGame(){
+    
+    // set the number of rounds played as well as reset wins and losess
+    let roundCount = 0;
+    winCount = 0;
+    loseCount = 0;
 
-console.log(playRound(playerSelection));
+    // five rounds
+    while(roundCount < 5){
+    
+        playerSelection = prompt("Rock, Paper, or Scissors", "Rock");
+    
+        // play a round
+        outputMessage = playRound(playerSelection);
+
+        // output for user
+        alert(outputMessage +" You've won " + winCount + " rounds and lost " + loseCount + " rounds.");
+        roundCount++;
+
+    }
+
+    // final wincount
+    alert("Game over, you won " + winCount + " out of 5 rounds!");
+
+
+}
+
+// play game on page start
+playGame();
